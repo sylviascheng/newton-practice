1086ac600fdc9e7b43f5ff4cd16212da75b6682a
sylviascheng <sylvia-cheng@berkeley.edu>
2024-08-22 13:22:05 -0700

    added Docstring to the optimization function

--- newton.py
+++ newton.py
@@ -3,8 +3,11 @@ def fun(x):
     output = x**2 - 4
     return output
 
-
 def newton_optimize(x):
+    """
+    This function takes derivates of a given global function wrt the input x, 
+    and stops when the criteria of the abs difference between x and x2 is met.
+    """
     epsilon = 0.1**5
     first_deriv = (fun(x + epsilon) - fun(x)) / epsilon
     x2 = x - x / first_deriv
