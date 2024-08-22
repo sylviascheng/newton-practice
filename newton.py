39757882b6aed869dc2b60b12508d7ce298bc235
sylviascheng <sylvia-cheng@berkeley.edu>
2024-08-22 11:54:14 -0700

    used ruff

--- newton.py
+++ newton.py
@@ -1,16 +1,16 @@
-# newton practice 
-def fun(x): 
-    output = x**2 - 4 
-    return output 
-    
-def newton_optimize(x): 
-    epsilon = 0.1**5
-    first_deriv = (fun(x+epsilon) - fun(x))/epsilon 
-    x2 = x - x/first_deriv
+# newton practice
+def fun(x):
+    output = x**2 - 4
+    return output
 
-    while abs(x2 - x)>=0.01:
-        x = x2 
-        deriv = (fun(x+epsilon) - fun(x))/epsilon 
-        x2 = x - x/deriv
+
+def newton_optimize(x):
+    epsilon = 0.1**5
+    first_deriv = (fun(x + epsilon) - fun(x)) / epsilon
+    x2 = x - x / first_deriv
+
+    while abs(x2 - x) >= 0.01:
+        x = x2
+        deriv = (fun(x + epsilon) - fun(x)) / epsilon
+        x2 = x - x / deriv
     return [x2, deriv]
-    
